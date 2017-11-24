52c39bc0d3bc165cb56f087070e93bef8cd56b83
Alchem1st <123qqq@biqlik.ru>
2017-11-24 14:49:52 +1000

    Паказывает баланс по каждой валюте плюс общую сумму

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 //	"github.com/Syfaro/telegram-bot-api"
 	"io/ioutil"
 	"log"
-	"net/http"
+//	"net/http"
 	"time"
 	"os"
-	"crypto/tls"
+//	"crypto/tls"
 	"gopkg.in/telegram-bot-api.v4"
+	coinApi "github.com/miguelmota/go-coinmarketcap"
 //	"strconv"
 //	"strings"
 //	"encoding/xml"
+//	"bytes"
 )
 
 //
@@ -26,12 +28,13 @@ import (
 //other statuses - crit
 
 var (
-	SiteList   map[string]int
+	SiteList   map[string]float64
 	botToken   map[string]interface{}
 	chatID     int64
 	telegramBotToken string
 	configFile string
 	configFileBot string
+	alarm string
 	HelpMsg    = "Это простой мониторинг доступности сайтов. Он обходит сайты в списке и ждет что он ответит 200, если возвращается не 200 или ошибки подключения, то бот пришлет уведомления в групповой чат\n" +
 		"Список доступных комманд:\n" +
 		"/site_list - покажет список сайтов в мониторинге и их статусы (про статусы ниже)\n" +
@@ -47,7 +50,7 @@ var (
 )
 
 func init() {
-	SiteList = make(map[string]int)
+	SiteList = make(map[string]float64)
 
 //	file, _ := os.Open("config_bot.json")
 //	decoder := json.NewDecoder(file)
@@ -85,7 +88,7 @@ func init() {
 func send_notifications(bot *tgbotapi.BotAPI) {
 	for site, status := range SiteList {
 		if status != 200 {
-			alarm := fmt.Sprintf("CRIT - %s ; status: %v", site, status)
+			alarm := fmt.Sprintf("CRIT - %s ; status: %.0f", site, status)
 			bot.Send(tgbotapi.NewMessage(chatID, alarm))
 		}
 	}
@@ -128,27 +131,38 @@ func load_list() {
 
 //	fmt.Println(databot)
 
-	fmt.Printf("тип: %T\n", botToken["TelegramBotToken"])
-	fmt.Printf("тип: %T\n", int64(botToken["chatID"].(float64)))
+//	fmt.Printf("тип: %T\n", botToken["TelegramBotToken"])
+//	fmt.Printf("тип: %T\n", int64(botToken["chatID"].(float64)))
 
 	log.Printf(string(data))
 }
 
+var summ float64
+
 func monitor(bot *tgbotapi.BotAPI) {
 
+//	var summ float64
+	/*
+	// параметр для http клиента чтобы принимал все ssl сертификаты
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
+
+	// важно указать таймаут http соединения, иначе он вечно будет висеть и мы не увидим
+	// проблемы если сервер просто упал
 
 	var httpclient = &http.Client{
 		Timeout: time.Second * 10,
 		Transport: tr,
 	}
-
+*/
+	// в вечном цикле обходим список урлов раз в 5 мин и сохраняем статус в глобальный map SiteList
 	for {
-		save_list()
+		// сохраняем текущий статус SiteList в файл configFile
+//		save_list()
+		var summ float64
 		for site, _ := range SiteList {
-			response, err := httpclient.Get(site)
+/*			response, err := httpclient.Get(site)
 			if err != nil {
 				SiteList[site] = 1
 				log.Printf("Status of %s: %s", site, "1 - Connection refused")
@@ -156,34 +170,126 @@ func monitor(bot *tgbotapi.BotAPI) {
 				log.Printf("Status of %s: %s", site, response.Status)
 				SiteList[site] = response.StatusCode
 			}
+*/			// Get info about coin
+			coinInfo, err := coinApi.GetCoinData(site)
+			if err != nil {
+				log.Println(err)
+			} else {
+				fmt.Printf(" %s: ($%.0f) %5.0f\n", site, coinInfo.PriceUsd, coinInfo.PriceRub * SiteList[site])
+				alarm = fmt.Sprintf(" %s: ($%.0f) %5.0f\n", site, coinInfo.PriceUsd, coinInfo.PriceRub * SiteList[site]) + alarm
+//				bot.Send(tgbotapi.NewMessage(chatID, alarm))
+			}
+			summ = (coinInfo.PriceRub * SiteList[site]) + summ
 		}
-		send_notifications(bot)
-		time.Sleep(time.Minute * 5)
+		fmt.Printf("Total: %5.0f руб.\n", summ)
+		alarm = fmt.Sprintf("Total: %5.0f\n", summ) + alarm
+		bot.Send(tgbotapi.NewMessage(chatID, alarm))
+
+		/*
+		// Get info about coin
+		coinInfo, err := coinApi.GetCoinData("bitcoin")
+		if err != nil {
+			log.Println(err)
+		} else {
+			fmt.Printf(" BTC: %5.0f\n", coinInfo.PriceRub * 0.05086186)
+		}
+
+		var summ = coinInfo.PriceRub * 0.05086186
+
+		// Get info about coin
+		coinInfo, err = coinApi.GetCoinData("monero")
+		if err != nil {
+			log.Println(err)
+		} else {
+			fmt.Printf(" XMR: %5.0f\n", coinInfo.PriceRub * 0.243)
+		}
+
+		summ = (coinInfo.PriceRub * 0.243) + summ
+		// Get info about coin
+		coinInfo, err = coinApi.GetCoinData("ethereum")
+		if err != nil {
+			log.Println(err)
+		} else {
+			fmt.Printf(" ETH: %5.0f\n", coinInfo.PriceRub * 0.1)
+		}
+		summ = (coinInfo.PriceRub * 0.1) + summ
+
+		// Get info about coin
+		coinInfo, err = coinApi.GetCoinData("dash")
+		if err != nil {
+			log.Println(err)
+		} else {
+			fmt.Printf("DASH: %5.0f\n", coinInfo.PriceRub * 0.12556303)
+		}
+		summ = (coinInfo.PriceRub * 0.12556303) + summ
+		fmt.Printf("Total: %5.0f руб.\n", summ)
+*/
+		// шлем нотификации
+//		send_notifications(bot)
+//		time.Sleep(time.Minute * 5)
+		time.Sleep(time.Hour * 12)
 	}
 }
 
+
 func main() {
+/*
+	// Get global market data
+	marketInfo, err := coinApi.GetMarketData()
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(marketInfo)
+	}
+*/
+/*
+	// Get info about coin
+	coinInfo, err := coinApi.GetCoinData("monero")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(coinInfo.PriceRub)
+	}
+
+	// Get info about coin
+	coinInfo, err = coinApi.GetCoinData("ethereum")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(coinInfo.PriceRub)
+	}
+
+	// Get info about coin
+	coinInfo, err = coinApi.GetCoinData("dash")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(coinInfo.PriceRub)
+	}
+*/
+
 	bot, err := tgbotapi.NewBotAPI(telegramBotToken)
 	if err != nil {
 		log.Panic(err)
 	}
-
+/*
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 	log.Printf("Config file: %s", configFile)
 	log.Printf("Config file: %s", configFileBot)
 	log.Printf("ChatID: %v", chatID)
 	log.Printf("Starting monitoring thread")
+*/
 	go monitor(bot)
 
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u.Timeout = 1
 
-	bot.Send(tgbotapi.NewMessage(chatID, fmt.Sprint("Я живой; вот сайты которые буду мониторить: ", SiteList)))
+	bot.Send(tgbotapi.NewMessage(chatID, fmt.Sprint("буду мониторить: ", SiteList)))
 
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		reply := ""
+		reply := "Не знаю что сказать"
 		if update.Message == nil {
 			continue
 		}
